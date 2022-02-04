881debd373a836e1e7224bfbe45f1ff31b18899d
Jon Fuller <jcfuller@sep.com>
2022-02-04 14:19:58 -0500

    add logging

--- HealthKitData.Core/Excel/ExcelReport.cs
+++ HealthKitData.Core/Excel/ExcelReport.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using HealthKitData.Core.Excel.Sheets;
+using Microsoft.Extensions.Logging;
 using NodaTime;
 using OfficeOpenXml;
 
@@ -11,29 +12,39 @@ namespace HealthKitData.Core.Excel
     {
         public static void BuildReport(IList<Record> records, IList<Workout> workouts, ExcelWorkbook workbook, Settings.Settings settings, DateTimeZone zone, IEnumerable<ExcelWorksheet> customSheets)
         {
+            var logger = Logging.LoggerFactory.CreateLogger(typeof(ExcelReport));
+
             BuilderFactory.GetBuilders(settings, zone, records, workouts)
                 .Select(b => new { b.sheetName, b.builder })
+                .Select(b =>
+                {
+                    logger.LogDebug($"builder: {b.sheetName} -> {b.builder.GetType()}");
+                    return b;
+                })
+                .ToList()
                 .AsParallel().AsOrdered()
-                .Select(b => new { b.sheetName, data = GetData(b.builder) })
+                .Select(b => new { b.sheetName, data = GetData(b.builder, logger) })
                 .AsSequential()
                 .ToList().ForEach(s =>
                 {
                     var sheet = workbook.Worksheets.Add(s.sheetName);
-                    var wroteData = WriteData(sheet, s.data);
+                    var wroteData = WriteData(sheet, s.data, logger);
 
                     if (settings.OmitEmptySheets && !wroteData)
                     {
+                        logger.LogDebug($"removing empty sheet {sheet.Name}");
                         workbook.Worksheets.Delete(sheet);
                     }
                 });
 
+            logger.LogDebug($"placing custom sheets: {settings.CustomSheetsPlacement}");
             workbook.PlaceCustomSheets(
                 settings.CustomSheetsPlacement,
                 customSheets,
                 SheetNames.Summary);
         }
 
-        private static object GetData(object builder)
+        private static object GetData(object builder, ILogger logger)
         {
             var builderTypes = builder
                 .GetType()
@@ -45,10 +56,11 @@ namespace HealthKitData.Core.Excel
             var openGetSheet = typeof(ExcelReport).GetMethod(nameof(GetRawSheetTyped), BindingFlags.Static | BindingFlags.NonPublic);
             var closedGetSheet = openGetSheet.MakeGenericMethod(builderTypes);
 
+            logger.LogDebug($"getting data for {builder.GetType()}");
             return closedGetSheet.Invoke(null, new[] {builder});
         }
 
-        private static bool WriteData(ExcelWorksheet sheet, object data)
+        private static bool WriteData(ExcelWorksheet sheet, object data, ILogger logger)
         {
             var builderTypes = data
                 .GetType()
@@ -57,6 +69,7 @@ namespace HealthKitData.Core.Excel
             var openWriteSheet = typeof(ExcelReport).GetMethod(nameof(WriteSheetTyped), BindingFlags.Static | BindingFlags.NonPublic);
             var closedWriteSheet = openWriteSheet.MakeGenericMethod(builderTypes);
 
+            logger.LogDebug($"writing data for {sheet.Name}");
             return (bool)closedWriteSheet.Invoke(null, new[] { data, sheet });
         }
 
---
+++ HealthKitData.Core/Logging.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HealthKitData.Core
+{
+    public static class Logging
+    {
+        public static ILoggerFactory LoggerFactory { get; private set; } = Microsoft.Extensions.Logging.LoggerFactory.Create(
+            b => b.SetMinimumLevel(LogLevel.Information));
+
+        public static void Configure(ILoggerFactory loggerFactory)
+        {
+            LoggerFactory = loggerFactory;
+        }
+    }
+}
